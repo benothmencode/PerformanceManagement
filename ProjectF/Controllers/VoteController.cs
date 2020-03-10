580546d5f69d008c408f3b6a7cc4c1df9202a4d8
wijden ben othman <wbo@iyasoft.corp>
2020-03-10 16:32:39 +0100

    voteEmployeesPages

---
+++ ProjectF/Controllers/VoteController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectF.Models;
+
+namespace ProjectF.Controllers
+{
+    public class VoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            var votes = new List<Vote>();
+            votes.Add(new Vote() { Id = 1, Title = "helpful", Quantity = 3 });
+            votes.Add(new Vote() { Id = 1, Title = "Smart", Quantity = 1 });
+
+            return View(votes);
+        }
+
+        
+
+        //public User offerVote(int idVote)
+        //{
+
+        //}
+    }
+}

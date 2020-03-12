1e71267a5812f2d34a7ba101b95d1d484e03efcf
Racha Aoun <raa@iyasoft.corp>
2020-03-12 15:42:30 +0100

    nn

--- ProjectF/Startup.cs
+++ ProjectF/Startup.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using PerformanceManagement.DATA.DbContexts;
 
 namespace ProjectF
 {
@@ -20,6 +21,8 @@ namespace ProjectF
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            string connectionString = this.Configuration.GetConnectionString("DefaultContext");
+            services.AddDbContext<PerformanceManagementDBContext>(Options => Options.UseSqlServer(connectionString), ServiceLifetime.Scoped);
 
 
         }

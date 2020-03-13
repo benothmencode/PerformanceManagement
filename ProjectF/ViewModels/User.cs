cad6680f213d8d77944b1854ba4162f8239ffef5
wijden ben othman <wbo@iyasoft.corp>
2020-03-13 11:21:47 +0100

    newchanges

--- ProjectF/ViewModels/User.cs
+++ ProjectF/ViewModels/User.cs
@@ -5,7 +5,11 @@ namespace ProjectF.Models
     public class User
     {
         public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Username { get; set; }
+        public string Email { get; set; }
         public string Userimage { get; set; }
         public string Job { get; set; }
         public string Description { get; set; }
@@ -13,8 +17,7 @@ namespace ProjectF.Models
         public string Location { get; set; }
         public string Skills { get; set; }
 
-        public IList<Badge> Badges { get; set; }
-        public IList<Vote> Votes { get; set; }
+     
 
 
     }
